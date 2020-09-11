32dfd2d4cea18bc934d0cb20a9c33b6ca749f5fb
Shamik H <shamik05@hotmail.com>
2020-09-10 21:17:11 -0400

    Created config file for promise based mysql

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+const util = require("util");
+
+function makeDb() {
+  const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "&_Q32y^HuOwp",
+  database: "employeesDB",
+  });
+
+  return {
+    query( sql, args ) {
+      return util.promisify( connection.query )
+        .call( connection, sql, args );
+    },
+    close() {
+      return util.promisify( connection.end ).call( connection );
+    }
+  };
+}
+// Initiate MySQL Connection.
+
+module.exports = makeDb();
